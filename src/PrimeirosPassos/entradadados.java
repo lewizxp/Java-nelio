6a4c975847573e6f6e2d31b71adedd97f8d0ee07
lewizxp <luizpedrosantos297@gmail.com>
2025-03-14 19:54:44 -0300

    Scanner aprendido

---
+++ src/PrimeirosPassos/entradadados.java
@@ -0,0 +1,28 @@
+package PrimeirosPassos;
+import java.util.Locale;
+import java.util.Scanner;
+public class entradadados {
+
+    public static void main (String[] args){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner  (System.in);
+
+        char x ;
+         x= sc.next().charAt(1);
+        System.out.println("A Segunda Letra é " +x);
+
+
+
+        double n1 ;
+        double n2;
+        n1 = sc.nextDouble();
+        n2 = sc.nextDouble ();
+        double soma = n1 +n2;
+        System.out.println("o resultado de "+n1+ " + " +n2+ " é " +soma );
+
+
+        sc.close ();
+    }
+}
---
+++ src/PrimeirosPassos/expressosAri.java
@@ -0,0 +1,44 @@
+package PrimeirosPassos;
+
+import java. util.Locale;
+
+public class expressosAri {
+
+    public static void main (String[] args) {
+
+      String nome = "Luiz" ;
+      int idade = 21;
+      double salario = 6000.0;
+
+        System.out.printf("%s tem %d anos e ganha % 2f reais %n",nome,idade,salario);
+
+
+
+        String produto1 = "computador";
+        String produto2 = "teclado";
+
+        int idadess = 21;
+        int  code = 77888;
+        char genero = 'M';
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        Locale defaultLocale = Locale.US;
+        double measure = 53.234567;
+
+
+        System.out.printf("Produtos %n %s , vale %2f %n  , %s , vale %2f %n recorde : %d anos, o codigo é %d  e o genero é %c %n  o nummero com oito  decimos depois da virgula %8f %n  agora com 3 decimmais é assim %.3f %n   "
+                , produto1,price1 , produto2, price2,idadess,code,genero , measure,measure);
+
+        System.out.printf("em dolares fica assim %.2f" ,measure) ;
+
+
+int n1 = 10;
+double n2 = 20.0;
+double conta = n1 + n2;
+
+        System.out.println( "o resultado é "  + (double) conta);
+
+    }
+
+}
+
