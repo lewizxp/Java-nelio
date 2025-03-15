bfc0478c60ff1e95bba62b6796c72e5e618fa62c
lewizxp <luizpedrosantos297@gmail.com>
2025-03-15 16:49:49 -0300

    potencia feita

---
+++ src/FuncoesMath/Potencia.java
@@ -0,0 +1,27 @@
+package FuncoesMath;
+import java.util.Scanner;
+
+public class Potencia {
+    public static void main (String [] args){
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Digite a base da potência ");
+        double base;
+        base = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Digite um expoente");
+        double expoente;
+        expoente = sc.nextDouble();
+
+      double resultado=  Math.pow(base, expoente);
+
+        System.out.println("A potência de " +base+ " com expoente " +expoente+" da o resultado de "+resultado
+        );
+
+
+
+sc.close();
+    }
+}
