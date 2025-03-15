4760edc2b90a264149161568b401aedfa97c5b30
lewizxp <luizpedrosantos297@gmail.com>
2025-03-15 16:49:49 -0300

    potencia feita

---
+++ src/FuncoesMath/RaizQuadrada.java
@@ -0,0 +1,24 @@
+package FuncoesMath;
+
+
+import java.util.Scanner;
+
+
+public class RaizQuadrada {
+    public static void main (String [] args){
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Digite um número");
+        double n1;
+        n1=sc.nextDouble();
+
+
+       double raiz = Math.sqrt(n1);
+
+        System.out.println("A raiz quadrada de " +n1+ " é " +raiz) ;
+
+sc.close();
+    }
+
+}
