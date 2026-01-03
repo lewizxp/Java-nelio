22a436e3e71e358eae79ae1c18b6cf18ca3d71cf
lewizxp <luizpedrosantos297@gmail.com>
2026-01-02 23:07:36 -0300

    primeiro do ano

--- src/ExpressoesLogicas/ParImpar.java
+++ src/ExpressoesLogicas/ParImpar.java
@@ -2,20 +2,17 @@ package ExpressoesLogicas;
 import java.util.Scanner;
 
 public class ParImpar {
-    public static void main  (String [] args){
-
-        Scanner sc =  new Scanner (System.in);
-        System.out.println("Digite um número");
-
-        int n1 = sc.nextInt();
-
-        if(n1 % 2 ==0){
-
-            System.out.println("n1 é par");
-        }   else{
-            System.out.println("Esse número é ímpar");
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.printf("Digite um número");
+         double n1 = sc.nextDouble();
+        if (n1 % 2 == 0) {
+            System.out.printf("O número é par");
+        } else if (n1 == 0) {
+            System.out.printf("O numero é igual a zero");
+        }else {
+            System.out.printf("Seu número é ímpar");
         }
         sc.close();
     }
-
 }
