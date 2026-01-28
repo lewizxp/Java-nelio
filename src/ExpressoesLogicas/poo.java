830629a7385227a1476b9f967280b4e947599e1b
lewizxp <luizpedrosantos297@gmail.com>
2026-01-27 22:24:47 -0300

    teste feito

--- src/ExpressoesLogicas/poo.java
+++ src/ExpressoesLogicas/poo.java
@@ -1,4 +1,28 @@
 package ExpressoesLogicas;
 
+import java.util.Scanner;
+
 public class poo {
+
+
+    public class compidade {
+        public static void main (String [] args ){
+            Scanner sc= new Scanner(System.in);
+            System.out.printf("Digite a sua idade ");
+            int idade = sc.nextInt();
+
+            System.out.println("Sua idade é " +idade);
+
+            if(idade >= 20){
+                System.out.println("Você é mais velho  ");
+
+            } else if (idade <= 20) {
+                System.out.println("Você é um adoslecente ");
+
+            } else if (idade < 0){
+                System.out.println("Você não existe " );
+            }
+        }
+    }
+
 }
